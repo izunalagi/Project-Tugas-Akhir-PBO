2e7dbc7acc66d0718e9a859e25b53398fd4414f3
izuna_aja <116055126+izunalagi@users.noreply.github.com>
2024-05-24 08:35:45 +0700

    Login Register
    penanggungjawab : Novita,Abel,Panji dan Alfi

---
+++ login and register/model/model.cs
@@ -0,0 +1,44 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coba2.model
+{
+    internal class akun
+    {
+        public string username;
+        public string password;
+        public string no_telepon;
+        public DateTime tgl_masuk;
+
+        public void register()
+        {
+            NpgsqlConnection koneksi = new NpgsqlConnection("Host=localHost;Username=postgres;Password=alfi2265;Database=pbo tugas akhir");
+            koneksi.Open();
+
+            NpgsqlCommand perintah = new NpgsqlCommand($"insert into akun (username, password, no_telepon, tgl_masuk, id_role) values ('{this.username}','{this.password}','{this.no_telepon}','{tgl_masuk}','2')", koneksi);
+
+            perintah.ExecuteNonQuery();
+            koneksi.Close();
+        }
+
+        public bool login()
+        {
+            NpgsqlConnection con = new NpgsqlConnection("Host=localhost;Username=postgres;Password=alfi2265;Database=pbo tugas akhir");
+            con.Open();
+
+           
+            NpgsqlCommand cmd = new NpgsqlCommand($"SELECT username, password FROM akun WHERE username = '{this.username}' AND password = '{this.password}'", con);
+            
+            NpgsqlDataReader reader = cmd.ExecuteReader();
+            bool loginSuccessful = reader.HasRows;
+            reader.Close();
+            con.Close();
+
+            return loginSuccessful;
+        }
+    }
+}
---
+++ login and register/view/Login.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace login_and_register
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.Label();
+            this.Username = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button1.Location = new System.Drawing.Point(417, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.label1.Location = new System.Drawing.Point(226, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(348, 46);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "BATIK KARIMATA";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(335, 237);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(239, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(335, 196);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(239, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Password.Location = new System.Drawing.Point(225, 237);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(78, 20);
+            this.Password.TabIndex = 7;
+            this.Password.Text = "Password";
+            // 
+            // Username
+            // 
+            this.Username.AutoSize = true;
+            this.Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Username.Location = new System.Drawing.Point(225, 196);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(83, 20);
+            this.Username.TabIndex = 6;
+            this.Username.Text = "Username";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button2.Location = new System.Drawing.Point(293, 304);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 34);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Login";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.Label Username;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
